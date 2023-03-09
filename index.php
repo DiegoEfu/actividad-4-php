06de3e08ddd7b7f7dce94c4ccc5202a03cd17d30
Diego Faria <diegoafbrito12@gmail.com>
2023-03-08 23:23:11 -0400

    commit inicial

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PDVSA</title>
+</head>
+<body>
+    <h3>Lista de Pozos</h3>
+    <a href="#">Añadir Pozo</a>
+    <table border="2">
+        <thead>
+            <th># Pozo</th>
+            <th>Nombre</th>
+            <th>Ubicación</th>
+            <th>Última Medición</th>
+        </thead>
+        <tbody>
+            <td>1</td>
+            <td>Nombre</td>
+            <td>Lugar</td>
+            <td>Hoy</td>
+        </tbody>
+    </table>
+</body>
+</html>
