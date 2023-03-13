a14c218659a0385d35c3cb43ccefb6619cdd4543
Diego Faria <diegoafbrito12@gmail.com>
2023-03-13 11:32:45 -0400

    añadir consultas

--- index.php
+++ index.php
@@ -9,65 +9,80 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PDVSA</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+    <style>
+        body{
+            background-image: url('img/bg.jpg');
+            background-repeat: no-repeat;
+            background-size: cover;
+        }
+    </style>
 </head>
 <body>
-    <h3>Lista de Pozos</h3>
-    <a href="crear_pozo.php">Añadir Pozo</a>
-    <table border="2">
-        <thead>
-            <th>No.Pozo</th>
-            <th>Nombre</th>
-            <th>Estado</th>
-            <th>Ubicación</th>
-            <th>Mediciones</th>
-            <th>Editar</th>
-            <th>Eliminar</th>
-        </thead>
-        <tbody>
-        <?php
-                    
-            $query = "SELECT pozo.id AS id,pozo.nombre AS nombre,st.nombre AS estado_nombre,ubicacion FROM pozo INNER JOIN estado AS st ON pozo.estado = st.id ORDER BY -pozo.id";
-            $result = mysqli_query($conn, $query);
-    
-            while($row = mysqli_fetch_array($result)){?>
-                <tr>
-                    <td>
-                        <?php                                
-                            echo $row['id'];                                
-                        ?>
-                    </td>
-                    <td>
-                        <?php                                
-                            echo $row['nombre'];                             
-                        ?>
-                    </td>
-                    <td>
-                        <?php
-                            echo $row['estado_nombre'];
-                        ?>
-                    </td>
-                    <td>
-                        <?php
-                            echo $row['ubicacion'];
-                        ?>
-                    </td>
-                    <td>
-                        <form action="ver_mediciones.php" method="get">
-                            <input type="hidden" name="id_pozo" value="<?php echo $row['id']; ?>">
-                            <button name="pozo" value="<?php echo $row['nombre']; ?>" class="btn btn-outline-primary">
-                                Ver Mediciones
-                            </button>
-                        </form>
-                    </td>
-                    <td>
-                        <a href="editar_pozo.php?id=<?php echo $row['id'] ?>">Editar</a>
-                    </td>
-                    <td>
-                        <a href="eliminar_pozo.php?id=<?php echo $row['id'] ?>">Eliminar</a>
-                    </td>
-                </tr>
-            <?php } ?>
-        </tbody>
-    </table>
+    <div class="d-flex justify-content-center align-items-center bg-dark text-white flex-column">
+        <img src="img/pdvsa-logo.png" width="100px" alt="logo de PDVSA">
+        <h3>Lista de Pozos</h3>
+    </div>
+
+    <div class="m-3 d-flex flex-end flex-column">
+        <a href="crear_pozo.php" class="btn btn-danger">Añadir Pozo</a>
+        <table class="table table-hover">
+            <thead class="table-dark">
+                <th>No.Pozo</th>
+                <th>Nombre</th>
+                <th>Estado</th>
+                <th>Ubicación</th>
+                <th>Mediciones</th>
+                <th>Editar</th>
+                <th>Eliminar</th>
+            </thead>
+            <tbody class="table-light">
+            <?php
+                        
+                $query = "SELECT pozo.id AS id,pozo.nombre AS nombre,st.nombre AS estado_nombre,ubicacion FROM pozo INNER JOIN estado AS st ON pozo.estado = st.id ORDER BY -pozo.id";
+                $result = mysqli_query($conn, $query);
+        
+                while($row = mysqli_fetch_array($result)){?>
+                    <tr>
+                        <td>
+                            <?php                                
+                                echo $row['id'];                                
+                            ?>
+                        </td>
+                        <td>
+                            <?php                                
+                                echo $row['nombre'];                             
+                            ?>
+                        </td>
+                        <td>
+                            <?php
+                                echo $row['estado_nombre'];
+                            ?>
+                        </td>
+                        <td>
+                            <?php
+                                echo $row['ubicacion'];
+                            ?>
+                        </td>
+                        <td>
+                            <form action="ver_mediciones.php" method="get">
+                                <input type="hidden" name="id_pozo" value="<?php echo $row['id']; ?>">
+                                <button name="pozo" value="<?php echo $row['nombre']; ?>" class="btn btn-outline-danger">
+                                    Ver Mediciones
+                                </button>
+                            </form>
+                        </td>
+                        <td>
+                            <a class="btn btn-outline-danger" href="editar_pozo.php?id=<?php echo $row['id'] ?>">Editar</a>
+                        </td>
+                        <td>
+                            <a class="btn btn-outline-danger" href="eliminar_pozo.php?id=<?php echo $row['id'] ?>">Eliminar</a>
+                        </td>
+                    </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+    </div>    
 </body>
 </html>
--- ver_mediciones.php
+++ ver_mediciones.php
@@ -14,67 +14,85 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Consultar Mediciones</title>
     <script
-  src="https://code.jquery.com/jquery-3.6.4.js"
-  integrity="sha256-a9jBBRygX1Bh5lt8GZjXDzyOB+bWve9EiO7tROUtj/E="
-  crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    src="https://code.jquery.com/jquery-3.6.4.js"
+    integrity="sha256-a9jBBRygX1Bh5lt8GZjXDzyOB+bWve9EiO7tROUtj/E="
+    crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+    <style>
+        body{
+            background-image: url('img/bg.jpg');
+            background-repeat: no-repeat;
+            background-size: cover;
+        }
+    </style>
 </head>
 <body>
-<h3>Medidas del Pozo "<?php echo $_GET['pozo']; ?>"</h3>
-    <a href="index.php">Devolverse</a>
-    <canvas id="grafica" width="100%" style="max-height: 30vh;"></canvas>
-    <a href="registrar_medicion.php?id=<?php echo $_GET['id_pozo']; ?>&pozo=<?php echo $_GET['pozo']; ?>">Añadir Medición</a>
-    <table border="2">
-        <thead>
-            <th>No.Medida</th>
-            <th>Fecha</th>
-            <th>PSI</th>
-            <th>Registrada Por</th>
-            <th>Editar</th>
-            <th>Eliminar</th>
-        </thead>
-        <tbody>
-        <?php
-                    
-            $query = "SELECT * FROM medida WHERE pozo = " . $_GET['id_pozo'] . " ORDER BY -fecha;";
-            $result = mysqli_query($conn, $query);
+    <div class="d-flex justify-content-center align-items-center bg-dark text-white flex-column">
+        <img src="img/pdvsa-logo.png" width="100px" alt="logo de PDVSA">
+        <h3>Mediciones del Pozo "<?php echo $_GET['pozo']; ?>"</h3>
+    </div>
+    <canvas id="grafica" width="100%" style="max-height: 30vh; min-height: 30vh;" class="bg-white m-3"></canvas>
     
-            while($row = mysqli_fetch_array($result)){?>
-                <tr>
-                    <td>
-                        <?php                                
-                            echo $row['id'];                                
-                        ?>
-                    </td>
-                    <td>
-                        <?php                                
-                            echo $row['fecha'];                             
-                        ?>
-                    </td>
-                    <td>
-                        <?php
-                            echo $row['psi'];
-                        ?>
-                    </td>
-                    <td>
-                        <?php
-                            echo $row['creado_por'];
-                        ?>
-                    </td>
-                    <td>
-                        <a href="editar_medicion.php?id=<?php echo $row['id']; ?>&pozo=<?php echo $_GET['pozo']; ?>&id_pozo=<?php echo $_GET['id_pozo']; ?>" class="btn btn-outline-primary">
-                            Editar
-                        </a>
-                    </td>
-                    <td>
-                        <a href="eliminar_medicion.php?id=<?php echo $row['id']; ?>&pozo=<?php echo $_GET['pozo']; ?>&id_pozo=<?php echo $_GET['id_pozo']; ?>" class="btn btn-outline-primary">
-                            Eliminar
-                        </a>
-                    </td>
-                </tr>
-            <?php } ?>
-        </tbody>
-    </table>
+    <div class="m-3 d-flex flex-end flex-column">
+        <div>
+            <a href="index.php" class="btn btn-secondary">Regresar</a>
+            <a class="btn btn-danger" href="registrar_medicion.php?id=<?php echo $_GET['id_pozo']; ?>&pozo=<?php echo $_GET['pozo']; ?>">Añadir Medición</a>
+        </div>        
+        <table class="table table-hover">
+            <thead class="table-dark">
+                <th>No.Medida</th>
+                <th>Fecha</th>
+                <th>PSI</th>
+                <th>Registrada Por</th>
+                <th>Editar</th>
+                <th>Eliminar</th>
+            </thead>
+            <tbody class="table-light">
+            <?php
+                        
+                $query = "SELECT * FROM medida WHERE pozo = " . $_GET['id_pozo'] . " ORDER BY -fecha;";
+                $result = mysqli_query($conn, $query);
+        
+                while($row = mysqli_fetch_array($result)){?>
+                    <tr>
+                        <td>
+                            <?php                                
+                                echo $row['id'];                                
+                            ?>
+                        </td>
+                        <td>
+                            <?php                                
+                                echo $row['fecha'];                             
+                            ?>
+                        </td>
+                        <td>
+                            <?php
+                                echo $row['psi'];
+                            ?>
+                        </td>
+                        <td>
+                            <?php
+                                echo $row['creado_por'];
+                            ?>
+                        </td>
+                        <td>
+                            <a href="editar_medicion.php?id=<?php echo $row['id']; ?>&pozo=<?php echo $_GET['pozo']; ?>&id_pozo=<?php echo $_GET['id_pozo']; ?>" class="btn btn-outline-primary">
+                                Editar
+                            </a>
+                        </td>
+                        <td>
+                            <a href="eliminar_medicion.php?id=<?php echo $row['id']; ?>&pozo=<?php echo $_GET['pozo']; ?>&id_pozo=<?php echo $_GET['id_pozo']; ?>" class="btn btn-outline-primary">
+                                Eliminar
+                            </a>
+                        </td>
+                    </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+    </div>
 
     <script>
         $(document).ready(() => {
@@ -94,7 +112,7 @@
             });
 
             let myChart = new Chart(canvas, {
-                type: 'line',
+                type: 'bar',
                 data: {
                     datasets: [
                         {
